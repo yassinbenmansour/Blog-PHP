4f99345ed80b7ba8aeaad93bb47f64ab71d22432
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-03-22 23:20:37 +0000

    push project using script

---
+++ databse/connecxion.php
@@ -0,0 +1,16 @@
+<?php
+
+require('./const.php');
+
+$con = mysqli_connect(DB_HOST,DB_USER,DB_PASSWORD,DB_NAMEDATABASE);
+
+// if ($con) {
+//     echo "connected";
+// }else{
+//     echo "not connected";
+// }
+
+
+
+
+?>
---
+++ databse/const.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASSWORD','root');
+define('DB_NAMEDATABASE','php-cms');
+
+
+
+
+
+
+
+
+?>
