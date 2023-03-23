ea1fc9dd6e75a1025dffed32ebb5d3c28021190c
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-03-23 01:21:44 +0000

    push project using script

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+    require('./includes/header.php');
+?>
+
+<div class="container ">
+  <div class="row g-2">
+    <div class="col-lg-8 col-md-12 col-sm-12 border mt-5 rounded shadow">
+      <div class="p-3">
+            <div class="row">
+                <div class="col-4 ">
+                        <img
+        src="https://cdn.pixabay.com/photo/2015/05/31/15/07/coffee-792113_1280.jpg"
+        class="img-fluid rounded"
+        alt="Hollywood Sign on The Hill"
+        />
+                </div>
+                <div class="col-8">
+                    <h1 class="">Title</h1>
+                    <p class="">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nostrum necessitatibus nobis perferendis laudantium architecto enim ut facere magni animi id, aliquid illo ipsum cumque debitis eum vel magnam dolores excepturi.</p>
+                    <span class="badge rounded-pill badge-success ">Categorie</span>
+
+                </div>
+                
+            </div>
+        </div>
+    </div>
+
+    <div class="col-lg-4 col-md-12 col-sm-12 mt-5">
+        <div class="col-12 border rounded shadow">
+        <div class="p-3">Custom column padding</div>
+        </div>
+        <div class="col-12 border rounded shadow mt-3">
+        <div class="p-3">Custom column padding</div>
+        </div>
+    </div>
+
+    
+    
+  </div>
+</div>
