ecb0193ece4e6037cd936751ebd1adccd14b0021
yassine benmansour <99927722+yassinbenmansour@users.noreply.github.com>
2023-03-23 01:23:34 +0000

    push project using script

--- includes/header.php
+++ includes/header.php
@@ -68,24 +68,6 @@
 
     <!-- Right elements -->
     <div class="d-flex align-items-center">
-      <!-- Icon -->
-      <a class="text-reset me-3" href="#">
-        <i class="fas fa-shopping-cart"></i>
-      </a>
-
-      <!-- Notifications -->
-      <div class="dropdown">
-        <a
-          class="text-reset me-3 dropdown-toggle hidden-arrow"
-          href="#"
-          id="navbarDropdownMenuLink"
-          role="button"
-          data-mdb-toggle="dropdown"
-          aria-expanded="false"
-        >
-          <i class="fas fa-bell"></i>
-          <span class="badge rounded-pill badge-notification bg-danger">1</span>
-        </a>
         <ul
           class="dropdown-menu dropdown-menu-end"
           aria-labelledby="navbarDropdownMenuLink"
@@ -108,21 +90,6 @@
 </nav>
 <!-- Navbar -->
     
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 <!-- MDB -->
 <script
