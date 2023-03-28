c86c3174fd59d8d2c9eb7cb3dc3da0e859621e3e
Lazy benmansour <lazybenmansour@192.168.1.2>
2023-03-28 00:37:13 +0000

    push project using script

--- Database/tableE.sql
+++
@@ -1,9 +0,0 @@
-UPDATE ETUDIANT 
-SET  niveaEtu = 'BAC+5'  WHERE  niveaEtu = 'Master' ,
-SET  niveaEtu = 'BAC+4'  WHERE  niveaEtu = 'Bachelor' ,
-SET  niveaEtu = 'Doctorat'  WHERE  niveaEtu = 'Phd' ;
-
-
-
-UPDATE Formation
-SET  titre = 'developement java' WHERE  id = 11 
